cc0547301ba005c4153cf3e2dd5017213e001016
Saeed Nemati <sn.1361@gmail.com>
2021-12-07 15:48:34 +0330

    committed by script

--- Api/User/CountryController.cs
+++ Api/User/CountryController.cs
@@ -6,10 +6,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Holism.Geo.UserApi
 {
-    public class CountryController : Controller<Country, Country>
+    public class CountryController : ReadController<Country>
     {
         public override ReadBusiness<Country> ReadBusiness => new CountryBusiness();
-        
-        public override Business<Country, Country> Business => new CountryBusiness();
     }
 }
